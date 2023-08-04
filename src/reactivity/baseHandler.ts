272bf91af9756433c50834e96ff44c3bddac713f
xntzmk <hhb1298@gmail.com>
2023-08-04 11:54:12 +0800

    refactor: 重构 reactive & readonly

---
+++ src/reactivity/baseHandler.ts
@@ -0,0 +1,40 @@
+import { track, trigger } from './effect'
+
+const get = createGetter()
+const set = createSetter()
+const readonlyGet = createGetter(true)
+
+export function createGetter(isReadonly = false) {
+  return function (target: any, key: any) {
+    const res = Reflect.get(target, key)
+
+    // 依赖收集
+    if (!isReadonly)
+      track(target, key)
+
+    return res
+  }
+}
+
+function createSetter() {
+  return function (target: any, key: any, value: any) {
+    const res = Reflect.set(target, key, value)
+
+    // 触发依赖
+    trigger(target, key)
+
+    return res
+  }
+}
+
+export const mutableHandlers = {
+  get,
+  set,
+}
+
+export const readonlyHandlers = {
+  get: readonlyGet,
+  set(target: any, key: any, value: any) {
+    return true
+  },
+}
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,40 +1,13 @@
-import { track, trigger } from './effect'
-
-function createGetter(isReadonly = false) {
-  return function (target: any, key: any) {
-    const res = Reflect.get(target, key)
-
-    // 依赖收集
-    if (!isReadonly)
-      track(target, key)
-
-    return res
-  }
-}
-
-function createSetter() {
-  return function (target: any, key: any, value: any) {
-    const res = Reflect.set(target, key, value)
-
-    // 触发依赖
-    trigger(target, key)
-
-    return res
-  }
-}
+import { mutableHandlers, readonlyHandlers } from './baseHandler'
 
 export function reactive(raw: any) {
-  return new Proxy(raw, {
-    get: createGetter(),
-    set: createSetter(),
-  })
+  return createActiveObject(raw, mutableHandlers)
 }
 
 export function readonly(raw: any) {
-  return new Proxy(raw, {
-    get: createGetter(),
-    set(target, key, value) {
-      return true
-    },
-  })
+  return createActiveObject(raw, readonlyHandlers)
+}
+
+function createActiveObject(raw: any, baseHandlers: any) {
+  return new Proxy(raw, baseHandlers)
 }
