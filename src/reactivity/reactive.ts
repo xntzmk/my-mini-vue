df989576bdda4b3eb091ee3ce65c52165ac8676c
xntzmk <hhb1298@gmail.com>
2023-08-04 11:42:00 +0800

    feat: 实现 readonly 基础功能

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,23 +1,40 @@
 import { track, trigger } from './effect'
 
-export function reactive(raw: any) {
-  return new Proxy(raw, {
-    get(target, key) {
-      const res = Reflect.get(target, key)
+function createGetter(isReadonly = false) {
+  return function (target: any, key: any) {
+    const res = Reflect.get(target, key)
 
-      // 依赖收集
+    // 依赖收集
+    if (!isReadonly)
       track(target, key)
 
-      return res
-    },
+    return res
+  }
+}
 
+function createSetter() {
+  return function (target: any, key: any, value: any) {
+    const res = Reflect.set(target, key, value)
+
+    // 触发依赖
+    trigger(target, key)
+
+    return res
+  }
+}
+
+export function reactive(raw: any) {
+  return new Proxy(raw, {
+    get: createGetter(),
+    set: createSetter(),
+  })
+}
+
+export function readonly(raw: any) {
+  return new Proxy(raw, {
+    get: createGetter(),
     set(target, key, value) {
-      const res = Reflect.set(target, key, value)
-
-      // 触发依赖
-      trigger(target, key)
-
-      return res
+      return true
     },
   })
 }
