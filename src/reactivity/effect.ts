ac53c5240355e40d9ed2c1dfa902134ffe7f5984
xntzmk <hhb1298@gmail.com>
2023-08-04 10:41:25 +0800

    feat: 实现 effect 的 stop 功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,8 +1,13 @@
 /* eslint-disable @typescript-eslint/no-this-alias */
 
+import { extend } from '../shared'
+
 let activeEffect: any
 class ReactiveEffect {
+  [x: string]: any
   private _fn: any
+  deps = []
+  active = true
 
   constructor(fn: any, public scheduler?: any) {
     this._fn = fn
@@ -12,14 +17,32 @@ class ReactiveEffect {
     activeEffect = this
     return this._fn()
   }
+
+  stop() {
+    if (this.active) {
+      this.active = false
+      this.onStop && this.onStop()
+      cleanUpEffect(this)
+    }
+  }
+}
+
+function cleanUpEffect(effect: any) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect)
+  })
 }
 
 export function effect(fn: any, options: any = {}) {
-  const _effect = new ReactiveEffect(fn, options.scheduler)
+  const _effect = new ReactiveEffect(fn)
+  extend(_effect, options)
 
   _effect.run()
 
-  return _effect.run.bind(_effect)
+  const runner: any = _effect.run.bind(_effect)
+  runner.effect = _effect
+
+  return runner
 }
 
 const targetMaps = new Map()
@@ -36,7 +59,11 @@ export function track(target: any, key: any) {
     depMaps.set(key, dep)
   }
 
+  if (!activeEffect)
+    return
+
   dep.add(activeEffect)
+  activeEffect.deps.push(dep)
 }
 
 export function trigger(target: any, key: any) {
@@ -49,4 +76,8 @@ export function trigger(target: any, key: any) {
     else
       effect.run()
   }
+}
+
+export function stop(runner: any) {
+  runner.effect.stop()
 }
