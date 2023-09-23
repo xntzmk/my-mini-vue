d363f1c4912fe3d02bd20d0dab2635c9b6ad6a7a
xntzmk <hhb1298@gmail.com>
2023-09-23 09:59:54 +0800

    feat: 实现解析 element

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,5 +1,10 @@
 import { NodeTypes } from './ast'
 
+const enum TagType {
+  Start,
+  End,
+}
+
 export function baseParse(content: string) {
   const context = createParserContext(content) // 引入全局上下文
 
@@ -9,15 +14,49 @@ export function baseParse(content: string) {
 function parseChildren(context: any) {
   const nodes = []
 
+  const s = context.source
   let node
 
   // 解析插值
-  if (context.source.startsWith('{{'))
+  if (s.startsWith('{{')) {
     node = parseInterpolation(context)
+  }
+  // 解析元素
+  else if (s[0] === '<') {
+    if (/[a-z]/i.test(s[1]))
+      node = parseElement(context)
+  }
 
   nodes.push(node)
 
   return nodes
+}
+
+function parseElement(context: any) {
+  const element = parseTag(context, TagType.Start)
+
+  parseTag(context, TagType.End)
+
+  return element
+}
+
+function parseTag(context: any, type: TagType) {
+  // 1. 解析tag
+  const match: any = (/^<\/?([a-z]+)/i).exec(context.source)
+  const tag = match[1]
+
+  // 2. 推进
+  advanceBy(context, match[0].length) // 推进 tag+左尖括号(包括斜杠)
+  advanceBy(context, 1) // 推进 tag+右尖括号
+
+  // 结束标签直接返回
+  if (type === TagType.End)
+    return
+
+  return {
+    type: NodeTypes.ELEMENT,
+    tag,
+  }
 }
 
 function parseInterpolation(context: any) {
