00db94a335d10ad380e7d2628b3ba498666d8fde
xntzmk <hhb1298@gmail.com>
2023-09-24 09:51:04 +0800

    feat: 实现 transform 功能

---
+++ src/compiler-core/src/transform.ts
@@ -0,0 +1,34 @@
+export function transform(root: any, options: any) {
+  const context = createTransformContext(root, options)
+
+  // 深度优先遍历
+  traverseNode(root, context)
+}
+
+function createTransformContext(root: any, options: any) {
+  return {
+    root,
+    nodeTransforms: options.nodeTransforms || [],
+  }
+}
+
+function traverseNode(node: any, context: any) {
+  const { nodeTransforms } = context
+  for (let i = 0; i < nodeTransforms.length; i++) {
+    const transform = nodeTransforms[i]
+    transform(node)
+  }
+
+  traverseChildren(node, context)
+}
+
+function traverseChildren(node: any, context: any) {
+  const children = node.children
+
+  if (children) {
+    for (let i = 0; i < children.length; i++) {
+      const node = children[i]
+      traverseNode(node, context)
+    }
+  }
+}
